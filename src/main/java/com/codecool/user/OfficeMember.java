bc7f42352d3ac73c71397c8136238731dac6f114
pietia <piotrstepak@gmail.com>
2020-05-18 17:22:42 +0200

    add OfficeMember class

---
+++ src/main/java/com/codecool/user/OfficeMember.java
@@ -0,0 +1,8 @@
+package com.codecool.user;
+
+public class OfficeMember extends User {
+
+    public OfficeMember(int id, String name, String surname, String email, String password, String type) {
+        super(id, name, surname, email, password, type);
+    }
+}
