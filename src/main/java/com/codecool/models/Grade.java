c34be4f9a9d3c6567c59b142ebd13602b99c75c9
pietia <piotrstepak@gmail.com>
2020-05-18 17:33:00 +0200

    add Grade class

---
+++ src/main/java/com/codecool/models/Grade.java
@@ -0,0 +1,14 @@
+package com.codecool.models;
+
+public class Grade {
+    private int grade;
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+}
