f24f5ef06caa32a482b1e8dd7a47a964193148c0
pietia <piotrstepak@gmail.com>
2020-05-21 10:39:36 +0200

    Update methods

--- src/main/java/com/codecool/controllers/AdminController.java
+++ src/main/java/com/codecool/controllers/AdminController.java
@@ -1,7 +1,13 @@
 package com.codecool.controllers;
 
+import com.codecool.dao.UserDao;
+import com.codecool.models.UserTypes;
+import com.codecool.user.User;
 import com.codecool.utilities.InputProvider;
 import com.codecool.utilities.View;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class AdminController {
     private static AdminController instance;
@@ -18,20 +24,25 @@ public class AdminController {
         return instance;
     }
 
-    public void menu() {
+    public void menu() throws SQLException {
         boolean isRunning = true;
         while (isRunning){
             View.getInstance().showMenu(menu);
             switch (InputProvider.getInt("SELECT OPTION: ")){
                 case 1:
+                    getStudentsList();//view
                     break;
                 case 2:
+                    getMentorsList();//view
                     break;
                 case 3:
+                    addMentor();
                     break;
                 case 4:
+                    removeMentor();
                     break;
                 case 5:
+                    editMentorsData();
                     break;
                 case 0:
                     isRunning = false;
@@ -41,5 +52,33 @@ public class AdminController {
             }
 
         }
+    }
+
+    private List<User> getStudentsList() {
+        return UserDao.getInstance().getUsersByUserType(UserTypes.STUDENT);
+    }
+
+    private List<User> getMentorsList() {
+        return UserDao.getInstance().getUsersByUserType(UserTypes.MENTOR);
+    }
+
+    private void addMentor() throws SQLException {
+        String name = InputProvider.getString("Enter name: ");
+        String surname = InputProvider.getString("Enter surname: ");
+        String email = InputProvider.getString("Enter email: ");
+        String password = InputProvider.getString("Enter password");
+        UserDao.getInstance().addMentor(name, surname, email, password);
+    }
+
+    private void removeMentor() throws SQLException {
+        int mentorId = InputProvider.getInt("Enter mentors id: ");
+        UserDao.getInstance().removeUserById(mentorId);
+    }
+
+    private void editMentorsData() {
+        int mentorId = InputProvider.getInt("Enter mentors id: ");
+        String columnToEdit = InputProvider.getString("Enter column to edit: ");
+        String newParamData = InputProvider.getString("Enter new data: ");
+        UserDao.getInstance().editUserDataById(mentorId, "UserDetails", columnToEdit, newParamData);
     }
 }
--- src/main/java/com/codecool/controllers/MentorController.java
+++ src/main/java/com/codecool/controllers/MentorController.java
@@ -1,7 +1,12 @@
 package com.codecool.controllers;
 
+import com.codecool.dao.UserDao;
+import com.codecool.models.UserTypes;
+import com.codecool.user.User;
 import com.codecool.utilities.InputProvider;
 import com.codecool.utilities.View;
+
+import java.util.List;
 
 public class MentorController {
     private static MentorController instance;
@@ -25,6 +30,7 @@ public class MentorController {
             View.getInstance().showMenu(menu);
             switch (InputProvider.getInt("SELECT OPTION: ")){
                 case 1:
+                    getStudentsList();//view
                     break;
                 case 2:
                     break;
@@ -33,10 +39,13 @@ public class MentorController {
                 case 4:
                     break;
                 case 5:
+                    addStudentToClassroom();
                     break;
                 case 6:
+                    removeStudentFromClassroom();
                     break;
                 case 7:
+                    editStudentData();
                     break;
                 case 0:
                     isRunning = false;
@@ -46,5 +55,27 @@ public class MentorController {
             }
 
         }
+    }
+
+    private List<User> getStudentsList() {
+        return UserDao.getInstance().getUsersByUserType(UserTypes.STUDENT);
+    }
+
+    private void addStudentToClassroom() {
+        int studentId = InputProvider.getInt("Enter student id: ");
+        String newClassroom = InputProvider.getString("Enter new classroom name: ");
+        UserDao.getInstance().editUserDataById(studentId, "Students", "classroom", newClassroom);
+    }
+
+    private void removeStudentFromClassroom() {
+        int studentId = InputProvider.getInt("Enter student id: ");
+        UserDao.getInstance().editUserDataById(studentId, "Students", "classroom", null);
+    }
+
+    private void editStudentData() {
+        int studentId = InputProvider.getInt("Enter mentors id: ");
+        String columnToEdit = InputProvider.getString("Enter column to edit: ");
+        String newParamData = InputProvider.getString("Enter new data: ");
+        UserDao.getInstance().editUserDataById(studentId, "UserDetails", columnToEdit, newParamData);
     }
 }
