113c566e65bcb4d8ea5cfb4f6e62af947ff4472e
Patryk Krol <patryk.krol.98@gmail.com>
2020-05-21 10:13:02 +0200

    clean App.java

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,21 +1,11 @@
 package com.codecool;
 
-import com.codecool.dao.UserDao;
-import com.codecool.models.MenuController;
-import com.codecool.models.UserTypes;
-import com.codecool.user.Admin;
-import com.codecool.user.User;
-
-import java.util.List;
-
-import static com.codecool.models.UserTypes.MENTOR;
-import static com.codecool.models.UserTypes.STUDENT;
+import com.codecool.controllers.MenuController;
 
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(UserDao.getInstance().getUsersByUserType(MENTOR));
         MenuController menuController = new MenuController();
         while (menuController.isRunning) {
             menuController.mainMenu();
