9285ffd6f2f9ae858317336aa9218028db758fbd
Patryk Krol <patryk.krol.98@gmail.com>
2020-05-21 12:03:46 +0200

    make containers functionally

--- src/main/java/com/codecool/containers/UsersContainer.java
+++ src/main/java/com/codecool/containers/UsersContainer.java
@@ -1,30 +1,43 @@
 package com.codecool.containers;
 
-import com.codecool.models.UserTypes;
 import com.codecool.user.*;
+import com.codecool.utilities.InputProvider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class UsersContainer {
-    private List<User> userList;
+    private List<User> usersList;
     private List<User> studentsList;
     private List<User> mentorsList;
     private List<User> adminsList;
     private List<User> officeMembersList;
+    private static UsersContainer instance;
 
     public UsersContainer() {
-        this.userList = new ArrayList<>();
+        this.usersList = new ArrayList<>();
         this.officeMembersList = new ArrayList<>();
         this.studentsList = new ArrayList<>();
         this.mentorsList = new ArrayList<>();
         this.adminsList = new ArrayList<>();
     }
 
+    public static UsersContainer getInstance() {
+        if (instance == null) {
+            synchronized(InputProvider.class) {
+                if (instance == null) {
+                    instance = new UsersContainer();
+                }
+            }
+        }
+        return instance;
+    }
+
 
     public List<User> getStudentsList() {
         return studentsList;
-//        return studentsList.stream().filter(o -> o.getType().equals(UserTypes.STUDENT));
+//   TODO     return studentsList.stream().filter(o -> o.getType().equals(UserTypes.STUDENT));
     }
 
     public void setStudentsList(List<User> studentsList) {
@@ -56,10 +69,19 @@ public class UsersContainer {
     }
 
     public List<User> getUserList() {
-        return userList;
+        return usersList;
     }
 
     public void setUserList(List<User> userList) {
-        this.userList = userList;
+        this.usersList = userList;
+    }
+
+    public User getUserByEmailAndPassword(String email, String password) {
+        for (User user:usersList) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        throw new NoSuchElementException("There isn't user with specified data in database");
     }
 }
