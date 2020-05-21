c5996e1b0862b9504ca8cb8ddf6340f9e5b7cf6d
pietia <piotrstepak@gmail.com>
2020-05-21 10:08:52 +0200

    Update Controllers

--- src/main/java/com/codecool/models/MentorMenu.java
+++ src/main/java/com/codecool/models/MentorMenu.java
@@ -1,7 +1,11 @@
 package com.codecool.models;
 
+import com.codecool.dao.UserDao;
+import com.codecool.user.User;
 import com.codecool.utilities.InputProvider;
 import com.codecool.utilities.View;
+
+import java.util.List;
 
 public class MentorMenu {
     private static MentorMenu instance;
@@ -25,6 +29,7 @@ public class MentorMenu {
             View.getInstance().showMenu(menu);
             switch (InputProvider.getInt("SELECT OPTION: ")){
                 case 1:
+                    getStudentsList();//view
                     break;
                 case 2:
                     break;
@@ -33,8 +38,10 @@ public class MentorMenu {
                 case 4:
                     break;
                 case 5:
+                    addStudentToClassroom();
                     break;
                 case 6:
+                    removeStudentFromClassroom();
                     break;
                 case 7:
                     break;
@@ -46,5 +53,27 @@ public class MentorMenu {
             }
 
         }
+    }
+
+    private List<User> getStudentsList() {
+        return UserDao.getInstance().getUsersByUserType(UserTypes.STUDENT);
+    }
+
+    private void addStudentToClassroom() {
+        int studentId = InputProvider.getInt("Enter student id: ");
+        String newClassroom = InputProvider.getString("Enter new classroom name: ");
+        UserDao.getInstance().editUserDataById(studentId, "Students", "classroom", newClassroom);
+    }
+
+    private void removeStudentFromClassroom() {
+        int studentId = InputProvider.getInt("Enter student id: ");
+        UserDao.getInstance().editUserDataById(studentId, "Students", "classroom", null);
+    }
+
+    private void editStudentData() {
+        int studentId = InputProvider.getInt("Enter mentors id: ");
+        String columnToEdit = InputProvider.getString("Enter column to edit: ");
+        String newParamData = InputProvider.getString("Enter new data: ");
+        UserDao.getInstance().editUserDataById(studentId, "UserDetails", columnToEdit, newParamData);
     }
 }
